71d7131a92bb0337749bb9c3dfb08f474c12c3e0
שלמה <78599753+ShlomoCode@users.noreply.github.com>
2022-11-20 03:52:07 +0200

    הגדרת גלילה אוטומטית של דפים לראש בתצורת vue router

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import { useSnacksStore } from '@/stores/snacks'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior: () => ({ left: 0, top: 0, behavior: 'smooth' }),
   routes: [
     {
       path: '/',
